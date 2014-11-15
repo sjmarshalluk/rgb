eb7a3f6d566a8f41545f1cb3730e022821303b8b
Steve Marshall <steve@stevemarshalldesign.co.uk>
2014-11-15 13:41:23 +0000

    Undid precompile change and added edit page for breweries

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
 
   # Precompile additional assets. Defaults to [application.js, application.css, non-JS/CSS]
