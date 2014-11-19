669285011c31b5c6c7bea9f6119b4a129ffde561
Steve Marshall <steve@stevemarshalldesign.co.uk>
2014-11-19 08:22:33 +0000

    Removed some s3 stuff and hid img on list

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,7 +5,9 @@ class Brewery < ActiveRecord::Base
 	validates :user_email, presence: true
 
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.png"
+	has_attached_file :image, 
+		:styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+		:default_url => "/images/missing.png"
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   	geocoded_by :address
